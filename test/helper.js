b665772157e3f6d0e25858a584894cf6a6aaf52e
Samy Pessé <samypesse@gmail.com>
2015-01-30 12:47:16 +0100

    Base version

---
+++ test/helper.js
@@ -0,0 +1,8 @@
+// Nicety for mocha / Q
+global.qdone = function qdone(promise, done) {
+    return promise.then(function() {
+        return done();
+    }, function(err) {
+        return done(err);
+    }).done();
+};
